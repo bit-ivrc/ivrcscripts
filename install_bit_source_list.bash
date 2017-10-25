eb3e8e4e671ee705a9cbedbdd924ed7bdbf8caa9
yuzhangbit <yu.zhang.bit@gmail.com>
2017-10-25 09:52:10 +0800

    simplify the bit source list update script

--- install_bit_source_list.bash
+++ install_bit_source_list.bash
@@ -13,18 +13,29 @@ main()
    else
       echo "This is a script only for ubuntu 16.04."
    fi
-  
+
 }
 
 
 update_sourceslist()
 {
-   cd /etc/apt
-   sudo mv sources.list sources.list.bak
-   cd $SCRIPT_DIR
-   sudo cp -rf sources.list /etc/apt
-   sudo apt-get update 
-
+cd /etc/apt
+sudo mv sources.list sources.list.bak
+sudo touch sources.list
+echo "deb http://mirror.bit.edu.cn/ubuntu/ xenial main restricted universe multiverse
+deb http://mirror.bit.edu.cn/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirror.bit.edu.cn/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://mirror.bit.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
+##测试版源
+deb http://mirror.bit.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse
+# 源码
+deb-src http://mirror.bit.edu.cn/ubuntu/ xenial main restricted universe multiverse
+deb-src http://mirror.bit.edu.cn/ubuntu/ xenial-security main restricted universe multiverse
+deb-src http://mirror.bit.edu.cn/ubuntu/ xenial-updates main restricted universe multiverse
+deb-src http://mirror.bit.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
+##测试版源
+deb-src http://mirror.bit.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse" >> sources.list
+sudo apt-get update
 }
 
 main
