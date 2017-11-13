2e142f294457422d1831d31a5147004ac9370f9f
guanhaijie <32917030+guanhaijie@users.noreply.github.com>
2017-11-13 16:57:29 +0800

    Update latex_install.bash

--- latex_install.bash
+++ latex_install.bash
@@ -1,34 +1,45 @@
 #!/bin/bash
 set -e  # exit on first error
 
-echo "Installing TeXLive2017 for ubuntu 16.04."
+main()
+{
+  install_texLive2017()
+  
+  install_texStudio()
+}
 
-sudo wget -p /home/ http://mirror.bit.edu.cn/CTAN/systems/texlive/Images/texlive2017.iso 
+install_texLive2017()
+{
+  echo "Installing TeXLive2017 for ubuntu 16.04."
 
-cd /home/
+  sudo wget -p ~/http://mirror.bit.edu.cn/CTAN/systems/texlive/Images/texlive2017.iso 
 
-sudo mkdir /home/tex
+  mkdir ~/tex
 
-mount -t iso9660 texlive2017.iso -o rw /home/tex
+  mount -t iso9660 texlive2017.iso -o rw ~/tex
 
-cd tex
+  cd tex
 
-sudo ./install-tl
+  sudo ./install-tl
 
-sudo sed -i '$a\export MANPATH=${MANPATH}:/usr/local/texlive/2017/texmf-dist/doc/man' ~/.bashrc
+  sudo sed -i '$a\export MANPATH=${MANPATH}:/usr/local/texlive/2017/texmf-dist/doc/man' ~/.bashrc
 
-sudo sed -i '$a\export INFOPATH=${INFOPATH}:/usr/local/texlive/2017/texmf-dist/doc/info' ~/.bashrc
+  sudo sed -i '$a\export INFOPATH=${INFOPATH}:/usr/local/texlive/2017/texmf-dist/doc/info' ~/.bashrc
 
-sudo sed -i '$a\export PATH=${PATH}:/usr/local/texlive/2017/bin/x86_64-linux' ~/.bashrc
+  sudo sed -i '$a\export PATH=${PATH}:/usr/local/texlive/2017/bin/x86_64-linux' ~/.bashrc
 
-export PATH=$PATH:/usr/local/texlive/2017/bin/x86_64-linux
+  export PATH=$PATH:/usr/local/texlive/2017/bin/x86_64-linux
 
-echo "TeXLive is installed on your computer."
+  echo "TeXLive is installed on your computer."
 
-echo "Installing TeXStudio for ubuntu 16.04."
+  echo "Installing TeXStudio for ubuntu 16.04."
+}
 
-sudo sudo apt -qq install texstudio
+install_texStudio()
+{
+  sudo sudo apt -qq install texstudio
 
-echo "TeXStudio is installed on your computer."
+  echo "TeXStudio is installed on your computer."
+}
 
-exit 0
+main
