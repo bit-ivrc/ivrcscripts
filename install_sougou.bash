447cab3cfb6a55da56e3ccc9e5f41240e0abfba5
ChaoWang <2220170232@bit.edu.cn>
2017-12-12 21:39:06 +0800

    Update install_sougou.bash

--- install_sougou.bash
+++ install_sougou.bash
@@ -3,5 +3,6 @@ echo "Installing sogoupinyin for ubuntu 16.04."
 sudo sed -i '$a\deb http://archive.ubuntukylin.com:10006/ubuntukylin trusty main' /etc/apt/sources.list
 sudo apt-get -qq update
 sudo apt-get install sogoupinyin 
+sudo apt-get install fcitx-table-wubi
 echo "sogoupinyin has been installed in your computer"
 echo "If you can't find it,you need to restart your computer"
