eff2b843a2f33582ae52c5dbff5d0e9a28931191
Aidous <2220170232@bit.edu.cn>
2018-10-27 15:34:46 +0800

    update install sogou

--- latex_install.bash
+++ latex_install.bash
@@ -4,54 +4,54 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 main()
 {
-  install_texLive2017
+  install_texLive2018
   install_texStudio
 }
 
-install_texLive2017()
+install_texLive2018()
 {
-  echo "Installing TeXLive2017 for ubuntu 16.04."
+  echo "Installing TeXLive2018 for ubuntu 16.04."
  
-    # check if the texlive2017 is installed 
-  if [ -n "$(find /usr/local/texlive/2017/bin/x86_64-linux -name 'tex' | head -1)" ]; then
-     echo "The texlive2017 exists in '/usr/local/texlive'"
+    # check if the texlive2018 is installed 
+  if [ -n "$(find /usr/local/texlive/2018/bin/x86_64-linux -name 'tex' | head -1)" ]; then
+     echo "The texlive2018 exists in '/usr/local/texlive'"
   else 
-     echo "Installing texlive2017 to '/usr/local'........"
+     echo "Installing texlive2018 to '/usr/local'........"
      ## purge the old version texlive
      sudo apt-get purge texlive*
      mkdir -p ~/tex
      # check if a file exists by absolute paths
-     if [ -n "$(find ~/ -name 'texlive2017.iso' | head -1)" ]; then
-         echo "texlive2017.iso has already been downloaded."
+     if [ -n "$(find ~/ -name 'texlive2018.iso' | head -1)" ]; then
+         echo "texlive2018.iso has already been downloaded."
      else
-         echo "texlive2017.iso will be downloaded."
-         wget -P ~/ http://mirror.bit.edu.cn/CTAN/systems/texlive/Images/texlive2017.iso   
+         echo "texlive2018.iso will be downloaded."
+         wget -P ~/ http://mirror.bit.edu.cn/CTAN/systems/texlive/Images/texlive2018.iso   
      fi
 
      # check if the iso file has been mounted 	
      if mount | grep ~/tex > /dev/null; then
-         echo "The texlive2017.iso has already been mounted to ~/tex ."
+         echo "The texlive2018.iso has already been mounted to ~/tex ."
      else
-         echo "Mounting the texlive2017.iso to ~/tex."
-         sudo mount -t iso9660 ~/texlive2017.iso -o rw ~/tex
+         echo "Mounting the texlive2018.iso to ~/tex."
+         sudo mount -t iso9660 ~/texlive2018.iso -o rw ~/tex
      fi
      cd ~/tex
-     echo "Start to install the texlive2017 with a customized profile"
+     echo "Start to install the texlive2018 with a customized profile"
      sudo ./install-tl -profile $SCRIPT_DIR/ivrc.profile
      echo "TeXLive is installed on your computer."
      # configure the texlive path env
-     if ( grep 'export PATH=${PATH}:/usr/local/texlive/2017/bin/x86_64-linux' ~/.bashrc); then 
+     if ( grep 'export PATH=${PATH}:/usr/local/texlive/2018/bin/x86_64-linux' ~/.bashrc); then 
          echo "The texlive path env variables have been set." 
      else
-         echo "Set the path env for texlive2017.........."
+         echo "Set the path env for texlive2018.........."
      
-         sudo sed -i '$a\export MANPATH=${MANPATH}:/usr/local/texlive/2017/texmf-dist/doc/man' ~/.bashrc
+         sudo sed -i '$a\export MANPATH=${MANPATH}:/usr/local/texlive/2018/texmf-dist/doc/man' ~/.bashrc
 
-         sudo sed -i '$a\export INFOPATH=${INFOPATH}:/usr/local/texlive/2017/texmf-dist/doc/info' ~/.bashrc
+         sudo sed -i '$a\export INFOPATH=${INFOPATH}:/usr/local/texlive/2018/texmf-dist/doc/info' ~/.bashrc
 
-         sudo sed -i '$a\export PATH=${PATH}:/usr/local/texlive/2017/bin/x86_64-linux' ~/.bashrc
+         sudo sed -i '$a\export PATH=${PATH}:/usr/local/texlive/2018/bin/x86_64-linux' ~/.bashrc
 
-         sudo sed -i '$s/"$/:\/usr\/local\/texlive\/2017\/bin\/x86_64-linux"/' /etc/environment
+         sudo sed -i '$s/"$/:\/usr\/local\/texlive\/2018\/bin\/x86_64-linux"/' /etc/environment
      fi
   fi
 }
@@ -60,9 +60,9 @@ install_texLive2017()
 install_texStudio()
 { 
     echo "Installing TeXStudio for ubuntu 16.04........"
-    export MANPATH=${MANPATH}:/usr/local/texlive/2017/texmf-dist/doc/man
-    export INFOPATH=${INFOPATH}:/usr/local/texlive/2017/texmf-dist/doc/info
-    export PATH=${PATH}:/usr/local/texlive/2017/bin/x86_64-linux
+    export MANPATH=${MANPATH}:/usr/local/texlive/2018/texmf-dist/doc/man
+    export INFOPATH=${INFOPATH}:/usr/local/texlive/2018/texmf-dist/doc/info
+    export PATH=${PATH}:/usr/local/texlive/2018/bin/x86_64-linux
     echo "Remove the old version TexStudio......" 
     cd /tmp && sudo apt-get -y purge texstudio* && sudo dpkg -r texstudio
     cd $SCRIPT_DIR
--- sogou.bash
+++ sogou.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "Installing sogoupinyin for ubuntu 16.04."
 sudo sed -i '$a\deb http://archive.ubuntukylin.com:10006/ubuntukylin xenial main' /etc/apt/sources.list
-sudo apt-get -qq update
+sudo apt-get update
 sudo apt-get install sogoupinyin 
-sudo apt-get install fcitx-table-wubi
 echo "sogoupinyin has been installed in your computer"
 echo "If you can't find it,you need to restart your computer"
