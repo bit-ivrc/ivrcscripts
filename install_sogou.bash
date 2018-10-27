b6d4e312e3b09a71c62e1a9bc5b576d4c15087c1
Aidous <2220170232@bit.edu.cn>
2018-10-27 17:16:31 +0800

    complete sogoupinyin

--- install_sogou.bash
+++ install_sogou.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 GREEN_COLOR='\E[1;32m'
+YELOW_COLOR='\E[1;33m'
+WRITE_COLOR='\E[1;37m'
 echo "Installing sogoupinyin for ubuntu 16.04."
 sudo sed -i '$a\deb http://archive.ubuntukylin.com:10006/ubuntukylin xenial main' /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install sogoupinyin 
-<<<<<<< HEAD:install_sogou.bash
-echo -e  "${GREEN_COLOR}============================== Click "+" ==============================${RES}"
-echo -e  "${GREEN_COLOR}======Uncheck Only Show Current Language And Search "Sogou"!!!!!!======${RES}"
+echo -e  "${GREEN_COLOR}=============================  Click <<+>>  =============================${RES}"
+echo -e  "${GREEN_COLOR}===== Uncheck <<Only Show Current Language>> And Search <<Sogou>>!!! =====${RES}"
+echo -e  "${YELOW_COLOR}======= If you can't find it,you need to <<restart>> your computer ======${RES}"
+echo -e  "${YELOW_COLOR}================  And <<bash install_sogou.bash>> again!!!  ================${RES}"
+echo " "
+echo -e  "${WRITE_COLOR}#########################################################${RES}"
 cd /usr/bin/
 ./fcitx-configtool 
-=======
->>>>>>> 01e9a48c87bf920f920382e3ae769627d30cfa6d:install_sogou.bash
+
 echo "sogoupinyin has been installed in your computer"
-echo "If you can't find it,you need to restart your computer"
