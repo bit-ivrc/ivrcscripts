80b4bf91322f3e140bd7d584d877ce18f0ac3930
yuzhangbit <yu.zhang.bit@gmail.com>
2018-10-31 23:43:33 +0800

    add allow-unauthenticated

--- install_sogou.sh
+++ install_sogou.sh
@@ -7,7 +7,7 @@ WRITE_COLOR='\E[1;37m'
 echo "Installing sogoupinyin for ubuntu 16.04."
 sudo sed -i '$a\deb http://archive.ubuntukylin.com:10006/ubuntukylin xenial main' /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install -y sogoupinyin 
+sudo apt-get install -y sogoupinyin --allow-unauthenticated
 echo -e  "${GREEN_COLOR}=============================  Click <<+>>  =============================${RES}"
 echo -e  "${GREEN_COLOR}===== Uncheck <<Only Show Current Language>> And Search <<Sogou>>!!! =====${RES}"
 echo -e  "${YELOW_COLOR}======= If you can't find it,you need to <<restart>> your computer ======${RES}"
@@ -15,6 +15,6 @@ echo -e  "${YELOW_COLOR}================  And <<bash install_sogou.bash>> again!
 echo " "
 echo -e  "${WRITE_COLOR}#########################################################${RES}"
 cd /usr/bin/
-./fcitx-configtool 
+./fcitx-configtool
 
 echo "sogoupinyin has been installed in your computer"
