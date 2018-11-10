452f6f8ddde63cf1156745d12b939003f00adab4
Yu Zhang <yuzhangbit@users.noreply.github.com>
2018-11-10 17:52:51 +0800

    Cleanup the scripts (#17)
    * cleanup
    
    * simplify the scripts and cleanup dead codes
    
    * update
    
    * update script
    
    * fix format
    
    * update script
    
    * remove octomap install from source; remove travis build for it
    
    * remove unused parameter

--- install_ceres.sh
+++ install_ceres.sh
@@ -1,42 +1,35 @@
 #!/bin/bash
 set -e  # exit on first error
 UBUNTU_VERSION=`lsb_release --release | cut -f2`
-SRC_PREFIX_PATH="/usr/local/src/"
+SRC_PATH="/tmp"
 CERES_VERSION=1.12.0
 
 main(){
     if [ $UBUNTU_VERSION == "16.04" ]; then
         install_dependencies
         install_ceres_solver
-    fi   
+    fi
 }
 
 install_dependencies() {
-    apt-get update -qq
-    apt-get install -qq -y cmake \
+    sudo apt-get update -qq
+    sudo apt-get install -qq -y cmake \
                            libgoogle-glog-dev \
                            libatlas-base-dev \
                            libeigen3-dev \
                            libsuitesparse-dev
 }
 
-install_suitesparse_fix() {
-    add-apt-repository ppa:bzindovic/suitesparse-bugfix-1319687 -y
-    apt-get update -qq
-    apt-get install -qq libsuitesparse-dev
-}
 
 install_ceres_solver() {
-    mkdir -p $SRC_PREFIX_PATH
-    cd $SRC_PREFIX_PATH
-
+    cd $SRC_PATH
     # clone ceres-solver if directory does not already exist, or pull
     if [ ! -d ceres-solver ]; then
         git clone https://github.com/ceres-solver/ceres-solver.git
     else
     	cd ceres-solver
-    	git checkout master 
-    	git pull 
+    	git checkout master
+    	git pull
     	cd ..
     fi
 
--- install_geographic.sh
+++ install_geographic.sh
@@ -4,49 +4,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 main()
 {
-    echo "Installing geographiclib from source ........."
-    identify_environment
-    install_geographic_from_source
-}
-
-
-install_geographic_from_source()
-{
-    GEOGRAPHICLIB_VERSION="1.49"
-    GEOGRAPHICLIB_URL="https://sourceforge.net/projects/geographiclib/files/distrib/GeographicLib-$GEOGRAPHICLIB_VERSION.tar.gz"
-    GEOGRAPHICLIB_DIR="GeographicLib-$GEOGRAPHICLIB_VERSION"
-
-    if (ldconfig -p | grep -q libGeographic.so.17 ); then
-        echo "GeographicLib version $GEOGRAPHICLIB_VERSION is already installed."
-    else
-        echo "Installing GeographicLib version $GEOGRAPHICLIB_VERSION ..."
-        cd /tmp
-        rm -rf $GEOGRAPHICLIB_DIR
-        wget --no-check-certificate "$GEOGRAPHICLIB_URL"
-        tar -xf "GeographicLib-$GEOGRAPHICLIB_VERSION.tar.gz"
-        rm -rf "GeographicLib-$GEOGRAPHICLIB_VERSION.tar.gz"
-        cd "$GEOGRAPHICLIB_DIR"
-        mkdir -p BUILD
-        cd BUILD
-        cmake ..
-        make 
-        sudo make install > /dev/null
-    fi
-}
-
-# get UBUNTU_CODENAME, ROS_DISTRO, REPO_DIR, CATKIN_DIR
-identify_environment()
-{
-    UBUNTU_CODENAME=$(lsb_release -s -c)
-    case $UBUNTU_CODENAME in
-      xenial)
-        ROS_DISTRO=kinetic;;
-      *)
-        echo "Unsupported version of Ubuntu detected. Only xenial (16.04.*) are currently supported."
-        exit 1
-    esac
-    REPO_DIR=$(dirname "$SCRIPT_DIR")
-    CATKIN_DIR="$HOME/catkin_ws"
+    echo "Installing geographiclib....."
+    sudo apt-get update && sudo apt-get -y install libgeographic-dev
 }
 
 main
